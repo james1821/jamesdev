c8c06e47397fe48319a2cc0ec1cc3aebe712743f
james1821 <121045390+james1821@users.noreply.github.com>
2023-07-26 16:29:29 +0800

    changes the size fo the new image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,18 +6,18 @@ class About extends Component {
     const { MyData } = this.props;
     
     return (
-      <section id='About' className='h-[100vh] bg-white'>
+      <section id='About' className='h-[100vh] bg-[#000000]'>
         <div className="lg:flex-row flex-col ">
           <div className="lg:w-1/2 p-2">
-            <div className="text-center text-black">
+            <div className="text-center text-white">
               <h2 className='text-2xl font-bold pb-4'>About Me</h2>
               <p>{MyData.AboutMe}</p>
             </div>
           </div>
 
-          <div className="w-1/2 flex items-center justify-center">
+          <div className="w-full flex items-center justify-center">
             <div className="">
-              <img src={PointingImage} alt="Slideshow" className="w-[200px] h-[200px]" />
+              <img src={PointingImage} alt="Slideshow" className="w-[15rem] h-[30rem]" />
             </div>
           </div>
         </div>
