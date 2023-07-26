cb880c55b2c5543cce784b55160ea94053e42061
james1821 <121045390+james1821@users.noreply.github.com>
2023-07-27 02:46:38 +0800

    adding span to about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import PointingImage from './images/pointing_up.jpg';
 
 class About extends Component {
   render() {
-    const { MyData } = this.props;
+  
     
     return (
       <section id='About' className='h-full bg-[#000000]'>
@@ -13,7 +13,10 @@ class About extends Component {
           <div className="lg:w-1/2 p-2">
             <div className="text-center text-white">
               <h2 className='text-2xl font-bold pb-4 pt-4  '>About Me</h2>
-              <p className='md:text-4xl'>{MyData.AboutMe}</p>
+              <p className='md:text-4xl'>I am a fresh graduate of Bachelor of Science in Information Technology (BSIT) 
+              from Central Luzon State University (CLSU). 
+              <span className="text-custom-red"> I love learning </span> new technologies and <span className="text-custom-red">
+                building useful and cool things</span> with it.</p>
             </div>
           </div>
 
