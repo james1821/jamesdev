528f58839c3f0258b17092c1292a129779d7dcdb
james1821 <121045390+james1821@users.noreply.github.com>
2023-07-27 10:48:12 +0800

    fixed about section responsiveness

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,36 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PointingImage from './images/pointing_up.jpg';
 
 
-
-class About extends Component {
-  render() {
+const About = () => {
   
-    
-    return (
-      <section id='About' className='h-full bg-[#000000]'>
-        <div className="lg:flex-row flex-col ">
-          <div className="lg:w-1/2 p-2">
-            <div className="text-center text-white">
-              <h2 className='text-2xl font-bold pb-4 pt-4  '>About Me</h2>
-              <p className='md:text-4xl'>I am a fresh graduate of Bachelor of Science in Information Technology (BSIT) 
-              from Central Luzon State University (CLSU). 
-              <span className="text-custom-red"> I love learning </span> new technologies and <span className="text-custom-red">
-                building useful and cool things</span> with it.</p>
-            </div>
-          </div>
 
-          <div className="w-full flex items-center justify-center ">
-            <div className="">
-              <img src={PointingImage} alt="Slideshow" className="m-auto w-[60%] h-full" />
-            </div>
+  return (
+    <section id='About' className='h-full bg-[#000000]'>
+      <div className="lg:flex flex-column ">
+        <div className="lg:w-3/4 p-2 md:p-0">
+          <div className="text-center text-white lg:p-20 p-4">
+            <h2 className='text-2xl md:text-4xl font-bold pb-4 pt-4 text-custom-red '>About Me</h2>
+            <p className='md:text-3xl'>I am a fresh graduate of Bachelor of Science in Information Technology (BSIT) 
+            from Central Luzon State University (CLSU). <span className="text-custom-red"> I love learning </span>
+            new technologies and <span className="text-custom-red"> building useful and cool things</span> with it.</p>
           </div>
         </div>
         
-      
-      </section>
-    );
-  }
-}
+        <div className="w-full">
+          <img src={PointingImage} alt="Slideshow" className="m-auto w-[60%] md:w-[40%] h-full" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default About;
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default ({ mode }) => {
     build: {
       rollupOptions: {
         external: ['react-type-animation'],
-       external: ['react-scroll-parallax'],
+    
       },
     },
   });
