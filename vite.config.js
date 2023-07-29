4250698991d97178888a1506d49daaab5a1543ef
james1821 <121045390+james1821@users.noreply.github.com>
2023-07-30 02:28:04 +0800

    rollup

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default ({ mode }) => {
     build: {
       rollupOptions: {
         external: ['react-type-animation'],
-        external: ['@mui/lab/Timeline'],
+        external: ['react-awesome-reveal'],
     
       },
     },
