37c96aca3b137068addd555bc9e8b166d6ddb0e7
james1821 <121045390+james1821@users.noreply.github.com>
2023-07-30 17:55:49 +0800

    test

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Chrono } from "react-chrono";
+import { Slide } from "../../node_modules/react-awesome-reveal";
+import 'animate.css';
 
 
 const Education = () => {
@@ -31,7 +33,7 @@ const Education = () => {
    <section id='Education' className='bg-primary-bg h-full ' >
    <h1 className='text-center text-2xl md:text-4xl font-bold pb-4 pt-12 text-primary '>Education</h1>
 
-  
+
   <Chrono items={items} 
   mode="VERTICAL_ALTERNATING"
   scrollable={{ scrollbar: true }} 
@@ -44,7 +46,18 @@ const Education = () => {
      cardTitleColor: customRed,
    }}
 
-  />
+  >
+  <div>
+            <div style={{ width: "250px", height: "250px" }}>
+              <img
+                style={{ maxWidth: "100%", maxHeight: "100%" }}
+                alt="test"
+                src="https://cdn.tutsplus.com/net/uploads/2013/08/github-collab-retina-preview.gif"
+              />
+            </div>
+          </div>
+          
+      </Chrono>
 
         
    </section>
