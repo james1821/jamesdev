2656b4bf4dfaea0b3ad34b5601f9e8156c49a640
james1821 <121045390+james1821@users.noreply.github.com>
2023-07-31 01:08:07 +0800

    refactored code to improved readability, removed images

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,59 +1,58 @@
 import * as React from 'react';
-import { Chrono } from "react-chrono";
-import { Slide } from "../../node_modules/react-awesome-reveal";
-import crtImage from './images/crt.jpg';
-import collegeImage from './images/college.jpg';
+import { Chrono } from 'react-chrono';
+import { Slide } from 'react-awesome-reveal';
 import 'animate.css';
 
-
 const Education = () => {
-    const customRed = '#86C232'; 
-  
+  const customRed = '#86C232';
+
+ 
+  const timelineItems = [
+    {
+      title: '2017-2019',
+      cardTitle: 'CRT',
+      cardSubtitle: 'Subtitle for Card 1',
+      cardDetailedText: 'Details about Card 1',
+      
+    },
+    {
+      title: '2019-2023',
+      cardTitle: 'CLSU',
+      cardSubtitle: 'Subtitle for Card 1',
+      cardDetailedText: 'Details about Card 1',
+      
+    },
+   
+  ];
+
   return (
-   <section id='Education' className='bg-primary-bg h-full md:pt-56 ' >
-   <h1 className='text-center text-2xl md:text-4xl font-bold pb-4 pt-12 text-primary '>Education</h1>
+    <section id="Education" className="bg-primary-bg h-full">
+      <h1 className="text-center text-2xl md:text-4xl font-bold pb-4 pt-12 text-primary">Education</h1>
 
-
-  <Chrono 
-  mode="VERTICAL_ALTERNATING"
-  scrollable={{ scrollbar: true }} 
-   theme={{
-     primary: customRed,
-     secondary: customRed,
-     cardBgColor: 'grey',
-     titleColor: 'white',
-     titleColorActive: 'white',
-     cardTitleColor: customRed,
-   }}
-
-  >
-        <Slide> <div>
-            <div style={{ width: "100%", height: "100%" }}>
-              <img
-                style={{ maxWidth: "100%", maxHeight: "100%" }}
-                alt="test"
-                src={crtImage}
-              />
+      <Chrono
+        mode="VERTICAL_ALTERNATING"
+        scrollable={{ scrollbar: true }}
+        theme={{
+          primary: customRed,
+          secondary: customRed,
+          cardBgColor: 'grey',
+          titleColor: 'white',
+          titleColorActive: 'white',
+          cardTitleColor: customRed,
+        }}
+      >
+        {timelineItems.map((item, index) => (
+          <Slide key={index}>
+            <div>
+              <h2>{item.title}</h2>
+              <h3>{item.cardTitle}</h3>
+              <p>{item.cardSubtitle}</p>
             </div>
-          </div>
           </Slide>
-
-
-          <Slide> <div>
-            <div style={{ width: "100%", height: "100%" }}>
-              <img
-                style={{ maxWidth: "100%", maxHeight: "100%" }}
-                alt="test"
-                src={collegeImage}
-              />
-            </div>
-          </div>
-          </Slide>
+        ))}
       </Chrono>
+    </section>
+  );
+};
 
-        
-   </section>
-  )
-}
-
-export default Education
+export default Education;
