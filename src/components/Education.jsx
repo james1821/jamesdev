9b501b5b51beffe6c08c319c2bde4fed64273254
james1821 <121045390+james1821@users.noreply.github.com>
2023-07-30 18:26:46 +0800

    test build

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,24 +1,31 @@
+import * as React from 'react';
+import { Chrono } from "react-chrono";
+import { Slide } from "../../node_modules/react-awesome-reveal";
+import 'animate.css';
+
+
 const Education = () => {
-  const customRed = '#86C232'; 
+    const customRed = '#86C232'; 
   
   return (
-    <section id='Education' className='bg-primary-bg h-full'>
-      <h1 className='text-center text-2xl md:text-4xl font-bold pb-4 pt-12 text-primary'>Education</h1>
-      
-      <Chrono
-        mode="VERTICAL_ALTERNATING"
-        scrollable={{ scrollbar: true }}
-        theme={{
-          primary: customRed,
-          secondary: customRed,
-          cardBgColor: 'grey',
-          titleColor: 'white',
-          titleColorActive: 'white',
-          cardTitleColor: customRed,
-        }}
-      >
-   
-          <div>
+   <section id='Education' className='bg-primary-bg h-full ' >
+   <h1 className='text-center text-2xl md:text-4xl font-bold pb-4 pt-12 text-primary '>Education</h1>
+
+
+  <Chrono items={items} 
+  mode="VERTICAL_ALTERNATING"
+  scrollable={{ scrollbar: true }} 
+   theme={{
+     primary: customRed,
+     secondary: customRed,
+     cardBgColor: 'grey',
+     titleColor: 'white',
+     titleColorActive: 'white',
+     cardTitleColor: customRed,
+   }}
+
+  >
+        <Slide> <div>
             <div style={{ width: "250px", height: "250px" }}>
               <img
                 style={{ maxWidth: "100%", maxHeight: "100%" }}
@@ -27,11 +34,12 @@ const Education = () => {
               />
             </div>
           </div>
-       
-        
+          </Slide>
       </Chrono>
-    </section>
-  );
-};
 
-export default Education;
+        
+   </section>
+  )
+}
+
+export default Education
