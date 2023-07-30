d8af57ed16330be0febf145b69091b755eaad51b
james1821 <121045390+james1821@users.noreply.github.com>
2023-07-30 18:17:20 +0800

    test

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,28 +1,23 @@
-import * as React from 'react';
-import { Chrono } from "react-chrono";
-
-
 const Education = () => {
-    
+  const customRed = '#86C232'; 
+  
   return (
-   <section id='Education' className='bg-primary-bg h-full ' >
-   <h1 className='text-center text-2xl md:text-4xl font-bold pb-4 pt-12 text-primary '>Education</h1>
-
-
-  <Chrono 
-  mode="VERTICAL_ALTERNATING"
-  scrollable={{ scrollbar: true }} 
-   theme={{
-     primary: customRed,
-     secondary: customRed,
-     cardBgColor: 'grey',
-     titleColor: 'white',
-     titleColorActive: 'white',
-     cardTitleColor: customRed,
-   }}
-
-  >
-
+    <section id='Education' className='bg-primary-bg h-full'>
+      <h1 className='text-center text-2xl md:text-4xl font-bold pb-4 pt-12 text-primary'>Education</h1>
+      
+      <Chrono
+        mode="VERTICAL_ALTERNATING"
+        scrollable={{ scrollbar: true }}
+        theme={{
+          primary: customRed,
+          secondary: customRed,
+          cardBgColor: 'grey',
+          titleColor: 'white',
+          titleColorActive: 'white',
+          cardTitleColor: customRed,
+        }}
+      >
+        <Slide>
           <div>
             <div style={{ width: "250px", height: "250px" }}>
               <img
@@ -32,13 +27,11 @@ const Education = () => {
               />
             </div>
           </div>
-     
-          
-      </Chrono>
-
+        </Slide>
         
-   </section>
-  )
-}
+      </Chrono>
+    </section>
+  );
+};
 
-export default Education
+export default Education;
