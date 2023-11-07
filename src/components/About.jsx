35e91dcf049f7a8e58ceac6146da242a42d61733
james1821 <jamesespinosamark@.com>
2023-11-07 16:12:14 +0800

    Optimized imports and added fade animation on "About Me Text"

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import James from "/img/James.jpg";
-import { Slide } from "../../node_modules/react-awesome-reveal";
+import { Fade, Slide } from "react-awesome-reveal";
 import "animate.css";
 
 const About = () => {
@@ -8,22 +8,25 @@ const About = () => {
     <section id="About" className="h-full bg-[#000000]">
       <div className="lg:flex flex-row-reverse pt-20">
         <div className="lg:w-[90%] p-2  m-auto">
-          <div className="text-center text-white  p-4">
-            <h2 className="text-4xl md:text-6xl font-bold pb-4 pt-4 text-primary ">
-              About Me
-            </h2>
-            <p className="md:text-2xl ">
-              I am a fresh graduate of Bachelor of Science in Information
-              Technology (BSIT) from Central Luzon State University (CLSU).{" "}
-              <span className="text-primary"> I love learning </span>
-              new technologies and{" "}
-              <span className="text-primary">
-                {" "}
-                building useful and cool things
-              </span>{" "}
-              with it.
-            </p>
-          </div>
+          <Fade>
+            <div className="text-center text-white  p-4">
+              <h2 className="text-4xl md:text-6xl font-bold pb-4 pt-4 text-primary ">
+                About Me
+              </h2>
+
+              <p className="md:text-2xl ">
+                I am a fresh graduate of Bachelor of Science in Information
+                Technology (BSIT) from Central Luzon State University (CLSU).{" "}
+                <span className="text-primary"> I love learning </span>
+                new technologies and{" "}
+                <span className="text-primary">
+                  {" "}
+                  building useful and cool things
+                </span>{" "}
+                with it.
+              </p>
+            </div>
+          </Fade>
           <div className="flex justify-center items-center"></div>
         </div>
 
