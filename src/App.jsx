77b93c15aac08235527f64d2e56fe7902b3f3027
james1821 <jamesespinosamark@.com>
2023-11-07 22:36:50 +0800

    Added "Footer" component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Works from "./components/Projects/Works";
 import Exp from "./components/Exp";
 import Contacts from "./components/Contacts";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div>
@@ -14,6 +15,7 @@ function App() {
       <Works />
       <Exp />
       <Contacts />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="bg-[#000000] h-10">
+      <h1 className=" text-center text-1xl pt-2 md:5xl font-bold text-primary ">
+        © 2023 Mark James Espinosa
+      </h1>
+    </div>
+  );
+}
+
+export default Footer;
